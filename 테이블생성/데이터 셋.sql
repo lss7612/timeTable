069ddaf7eb9b0f2c7fe98cfb26e7b04d311ce595
lss7612 <lss76121@naver.com>
2022-01-24 00:50:00 +0900

    add query

---
+++ 테이블생성/데이터 셋.sql
@@ -0,0 +1,190 @@
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'C',
+        '1',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'C++',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'C#',
+        '3',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'Java',
+        '3',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'Scala',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'Kotlin',
+        '1',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'go',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'python',
+        '3',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'swift',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'Object-C',
+        '1',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'javaScript',
+        '3',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'typeScript',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'PHP',
+        '1',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'MySQL',
+        '3',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'MS-SQL',
+        '2',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'CUBRID',
+        '1',
+        2
+    );
+INSERT INTO TT_CLASS (
+        major_no,
+        class_name,
+        credit,
+        time
+    ) VALUES (
+        1,
+        'CLOUD',
+        '2',
+        2
+    );
+    
+INSERT INTO TT_MAJOR (major_no, major_name) VALUES (1,'컴퓨터공학과');
+commit;
